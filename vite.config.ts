8be03d9c2e98ce93e6f239401db4ef5a59940db1
patandrade09 <patricia.andrade094@gmail.com>
2023-10-16 00:56:25 -0300

    change deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/apod-nasa-version-2023"
+  base: "/apod-nasa-version-2023/"
 })
