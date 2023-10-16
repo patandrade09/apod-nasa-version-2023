d93019c98d9cfef94fcffd82501c312f0d00ccbf
patandrade09 <patricia.andrade094@gmail.com>
2023-10-16 11:55:33 -0300

    change route

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename={"/apod-nasa-version-2023/"}>
-    <Route element={<App />} path="/apod-nasa-version-2023" />
       <App />
     </BrowserRouter>
   </React.StrictMode>
